476ee39e181c2722c50b102a3192abf0b8870202
Woomymy <woomy@woomy.be>
2025-04-09 12:25:10 +0200

    viva: Sync with 03e825e5cfd3d7f5b210dc978fac550b8999db00
    Change-Id: I5ad621f1a27b87c6e4903d68e395d26030880c78
    Signed-off-by: Woomymy <woomy@woomy.be>

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),viva)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,79d8d206515d2401019618dd1f8129b84c30d0ee)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,80c1f97cb66d6b1e8dd7cd77cfd6ac823eea573a)
+$(call add-radio-file-sha1-checked,radio/gz.img,f6f1de95a7991dbeae63ade23e9afa69994945a1)
+$(call add-radio-file-sha1-checked,radio/lk.img,748a46ba41c10e5fe0883878458b1dddfa424290)
+$(call add-radio-file-sha1-checked,radio/logo.img,eb5562e4098a32ab2d0f233a7f0840c585657422)
+$(call add-radio-file-sha1-checked,radio/md1img.img,4a5e08758915d44d497fba25a6cbe844bafc3d52)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,a54413f34c2f35521fa9b3e6f321642b16624876)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,721ef9c516df8fbc4b67acf6e85dcc191dcdb961)
+$(call add-radio-file-sha1-checked,radio/scp.img,fd6de362445760841b941c1b5ca2cf274abb1528)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,660d32b806dcbe6ca0d654e7227d5cdb8e54eaf3)
+$(call add-radio-file-sha1-checked,radio/sspm.img,0e72ba9e5876dd0633937e86c7b68f69c2674b6f)
+$(call add-radio-file-sha1-checked,radio/tee.img,257ccfed0f0a2737b6e06e4226e702e4f2b968e1)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,17 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    audio_dsp \
+    dtbo \
+    gz \
+    lk \
+    logo \
+    md1img \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee
